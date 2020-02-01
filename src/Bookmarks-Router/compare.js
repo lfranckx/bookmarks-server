3eaa0b6caaad8dfcc426d11a5e0792b68336450f
Bowie <bowie@thinkful.com>
2020-02-01 10:22:23 -0800

    update

--- src/Bookmarks-Router/compare.js
+++ src/Bookmarks-Router/compare.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { isWebUri } = require('valid-url') // URL validation 
+const { isWebUri } = require('valid-url')
 const xss = require('xss')
 const logger = require('../logger')
 const BookmarksService = require('./bookmarks-service')
@@ -55,50 +55,50 @@ bookmarksRouter
     const newBookmark = { title, url, description, rating }
 
     BookmarksService.insertBookmark(
-      req.app.get('db'),
-      newBookmark
+        req.app.get('db'),
+        newBookmark
     )
-      .then(bookmark => {
-        logger.info(`Bookmark with id ${bookmark.id} created.`)
-        res
-          .status(201)
-          .location(`/bookmarks/${bookmark.id}`)
-          .json(serializeBookmark(bookmark))
-      })
-      .catch(next)
+        .then(bookmark => {
+            logger.info(`Bookmark with id ${bookmark.id} created.`)
+            res
+            .status(201)
+            .location(`/bookmarks/${bookmark.id}`)
+            .json(serializeBookmark(bookmark))
+        })
+        .catch(next)
   })
 
 bookmarksRouter
-  .route('/bookmarks/:bookmark_id')
-  .all((req, res, next) => {
-    const { bookmark_id } = req.params
-    BookmarksService.getById(req.app.get('db'), bookmark_id)
-      .then(bookmark => {
-        if (!bookmark) {
-          logger.error(`Bookmark with id ${bookmark_id} not found.`)
-          return res.status(404).json({
-            error: { message: `Bookmark Not Found` }
-          })
-        }
-        res.bookmark = bookmark
-        next()
-      })
-      .catch(next)
-  })
-  .get((req, res) => {
-    res.json(serializeBookmark(res.bookmark))
-  })
-  .delete((req, res, next) => {
-    const { bookmark_id } = req.params
-    BookmarksService.deleteBookmark(
-      req.app.get('db'),
-      bookmark_id
-    )
-      .then(numRowsAffected => {
-        logger.info(`Bookmark with id ${bookmark_id} deleted.`)
-        res.status(204).end()
-      })
-      .catch(next)
+    .route('/bookmarks/:bookmark_id')
+    .all((req, res, next) => {
+        const { bookmark_id } = req.params
+        BookmarksService.getById(req.app.get('db'), bookmark_id)
+        .then(bookmark => {
+            if (!bookmark) {
+            logger.error(`Bookmark with id ${bookmark_id} not found.`)
+            return res.status(404).json({
+                error: { message: `Bookmark Not Found` }
+            })
+            }
+            res.bookmark = bookmark
+            next()
+        })
+        .catch(next)
+    })
+    .get((req, res) => {
+        res.json(serializeBookmark(res.bookmark))
+    })
+    .delete((req, res, next) => {
+        const { bookmark_id } = req.params
+        BookmarksService.deleteBookmark(
+        req.app.get('db'),
+        bookmark_id
+        )
+        .then(numRowsAffected => {
+            logger.info(`Bookmark with id ${bookmark_id} deleted.`)
+            res.status(204).end()
+        })
+        .catch(next)
   })
 
 module.exports = bookmarksRouter
